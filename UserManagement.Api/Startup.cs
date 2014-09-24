fbe05f97c65de70db9be5ceef7bb9a174fe46e56
Jose Alonso Monge <josmonver@gmail.com>
2014-09-24 21:08:07 +0200

    Startup configuration

---
+++ UserManagement.Api/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(UserManagement.Api.Startup))]
+namespace UserManagement.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
